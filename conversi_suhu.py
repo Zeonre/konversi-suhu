ec1f97d5e7dbcd99b00ae78c29044359db577bb3
Zeonre <fideliskrisnagilangraharjO@gmail.com>
2025-03-09 23:17:03 +0700

    Update conversi_suhu.py

--- conversi_suhu.py
+++ conversi_suhu.py
@@ -1,5 +1,5 @@
-celcius_to_fahrenheit = lambda C: int((9/5) * C + 32)
-celcius_to_reamur = lambda C: int(0.8 * C)
+celcius_ke_fahrenheit = lambda C: int((9/5) * C + 32)
+celcius_ke_reamur = lambda C: int(0.8 * C)
 
 C = int(input("Masukkan suhu dalam Celcius: "))
 
